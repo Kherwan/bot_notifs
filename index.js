32b07b6a8418c6ba2a7203b6effd14f37f177a39
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-02 17:13:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,9 @@
 export default {
   async fetch(request, env) {
-    const response = await fetch(
-      "https://v3.football.api-sports.io/status",
-      {
-        headers: {
-          "x-apisports-key": env.API_FOOTBALL_KEY,,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      return new Response(
-        `Erreur API-Football : ${response.status}`,
-        { status: 500 }
-      );
+    if (!env.API_FOOTBALL_KEY) {
+      return new Response("CLÉ API ABSENTE", { status: 500 });
     }
 
-    const data = await response.json();
-
-    return new Response(
-      JSON.stringify(data),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    return new Response("CLÉ API PRÉSENTE ❤️🖤");
   },
 };
