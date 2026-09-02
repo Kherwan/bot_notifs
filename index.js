5e0b9f3c30da05f5ef851a424387ddd4db79145b
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-02 11:08:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 export default {
   async fetch(request, env) {
-    return new Response(
-      env.TEST_SECRET ? "TEST SECRET OK ❤️🖤" : "TEST SECRET ABSENT"
-    );
+    const response = await fetch(env.DISCORD_WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: "🤖 **Roazhon Parle est connecté !** ❤️🖤",
+      }),
+    });
+
+    if (!response.ok) {
+      return new Response(
+        `Erreur Discord : ${response.status}`,
+        { status: 500 }
+      );
+    }
+
+    return new Response("Message envoyé sur Discord ! ❤️🖤");
   },
 };
