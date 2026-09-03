bfc36f498e7efa3bda6eb54a1fdea2103d82d82f
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-03 17:08:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 export default {
   async fetch(request, env) {
+    const matchId = "db9b73aa-0cbb-4438-95e4-867ee91fb03d";
+
     const response = await fetch(
-      "https://api.bigballsdata.com/v1/matches?sport=football&league=ligue1",
+      `https://api.bigballsdata.com/v1/matches/${matchId}/events?sport=football`,
       {
         headers: {
           "x-api-key": env.BIGBALLS_API_KEY,
@@ -11,10 +13,13 @@ export default {
 
     const text = await response.text();
 
-    return new Response(text, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    return new Response(
+      `HTTP ${response.status}\n\n${text}`,
+      {
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+        },
+      }
+    );
   },
 };
