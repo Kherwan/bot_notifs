35b071e4294d46d7f4f8f3dfc4a433af3875d6ce
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-03 10:06:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const response = await fetch(
-      "https://v3.football.api-sports.io/fixtures?team=94&from=2026-09-03&to=2026-09-20",
+      "https://v3.football.api-sports.io/fixtures?team=94&season=2026&from=2026-09-03&to=2026-09-20",
       {
         headers: {
           "x-apisports-key": env.API_FOOTBALL_KEY,
