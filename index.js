1974666ff3b8078cf89d549912038ac76f6d31cd
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-03 10:22:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 export default {
   async fetch(request, env) {
     const response = await fetch(
-      "https://v3.football.api-sports.io/fixtures?team=94&season=2026&from=2026-09-03&to=2026-09-20",
+      "https://api.bigballsdata.com/v1/soccer/fixtures?league=61",
       {
         headers: {
-          "x-apisports-key": env.API_FOOTBALL_KEY,
+          "Authorization": `Bearer ${env.BIGBALLS_API_KEY}`,
+          "Content-Type": "application/json",
         },
       }
     );
@@ -20,4 +21,4 @@ export default {
       }
     );
   },
-};
+};   
