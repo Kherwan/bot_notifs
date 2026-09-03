8cf526e0b4b29df274e2de1cfeab81b32bb45fe1
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-03 09:59:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 export default {
   async fetch(request, env) {
     const response = await fetch(
+      "https://v3.football.api-sports.io/fixtures?team=94&from=2026-09-03&to=2026-09-20",
+      {
+        headers: {
+          "x-apisports-key": env.API_FOOTBALL_KEY,
+        },
+      }
+    );
+
+    const text = await response.text();
+
+    return new Response(
+      `HTTP ${response.status}\n\n${text}`,
+      {
+        headers: {
+          "Content-Type": "text/plain; charset=utf-8",
+        },
+      }
+    );
+  },
+};export default {
+  async fetch(request, env) {
+    const response = await fetch(
       "https://v3.football.api-sports.io/fixtures?team=94&next=1",
       {
         headers: {
