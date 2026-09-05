d47bd3f717204ca778d449978b327e9d35465c0f
Kherwan <kerwan.roulliaux@hotmail.fr>
2026-09-05 10:58:28 +0200

    Update API URL for fetching football fixtures

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const response = await fetch(
-      "https://v3.football.api-sports.io/fixtures?team=94&from=2026-09-06&to=2026-09-06",
+      "https://v3.football.api-sports.io/fixtures?team=94&season=2024",
       {
         headers: {
           "x-apisports-key": env.API_FOOTBALL_KEY,
